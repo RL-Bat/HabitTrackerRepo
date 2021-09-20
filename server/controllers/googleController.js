17d439b6a6aec7986d422329f1de16a7f54c7253
Carney <carneybernard23@gmail.com>
2021-09-19 22:26:52 -0400

    added dashboard router

--- server/controllers/googleController.js
+++ server/controllers/googleController.js
@@ -25,6 +25,7 @@ require("dotenv").config();
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const ENDCODED_SECRET = process.env.ENDCODED_SECRET;
 
 //from googleapi docs
 const oauth2Client = new google.auth.OAuth2(
@@ -38,46 +39,30 @@ googleController = {};
 googleController.login = (req, res) => {
   //create query params for request
   //scopes needed for access to user data
-  const scopes = [
-    "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/userinfo.email",
-    "https://www.googleapis.com/auth/calendar",
-  ];
-
-  //generate url using Google Api library
-  const url = oauth2Client.generateAuthUrl({
-    // 'online' (default) or 'offline' (gets refresh_token)
-    access_type: "offline",
-    scope: scopes,
-    response_type: "code",
-  });
-  return res.redirect(url);
-};
-
-//function used to check if new user needs to be create
-async function checkUser(currentUser) {
   try {
-    //check if user is currently in database
-    let user = await User.HabitsData.find({ userId: currentUser.user_id });
-    //if user is an empty array, user was not found so create new user and return user id
-    if (Array.isArray(user) && user.length === 0) {
-      //create new User query string out of passed in object
-      const newUserQuery = `http://localhost:3000/database/addUser?${queryString.stringify(
-        currentUser
-      )}`;
-      return newUserQuery;
-    } else {
-      //if user does exist return a query string with users id
-      const oldUserQuery = `http://localhost:3000/database/?${queryString.stringify(
-        currentUser.user_id
-      )}`;
-      return oldUserQuery;
-    }
-  } catch (err) {
-    return err;
-  }
-}
+    const scopes = [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/calendar",
+    ];
 
+    //generate url using Google Api library
+    const url = oauth2Client.generateAuthUrl({
+      // 'online' (default) or 'offline' (gets refresh_token)
+      access_type: "offline",
+      scope: scopes,
+      response_type: "code",
+    });
+    return res.redirect(url);
+  } catch (err) {
+    return res
+      .sendStatus(404)
+      .message(
+        "There was an error with your request to the server. Please try again"
+      );
+  }
+};
+//get user credentials
 googleController.getCredentials = async (req, res, next) => {
   try {
     //deconstruct query params for code
@@ -92,7 +77,7 @@ googleController.getCredentials = async (req, res, next) => {
         complete: true,
       });
 
-      //pull all pieces of data from decoded JWT  - sub is unique google id
+      //pull all pieces of data from decoded JWT  - sub is unique google account id
       const { sub, email, name, picture } = decoded.payload;
       //user obj with all values needed to create query param in createUser function
       const userObj = {
@@ -105,11 +90,12 @@ googleController.getCredentials = async (req, res, next) => {
       const databaseQuery = await checkUser(userObj);
 
       //create cookie for each new user session
-      const secret = randomString({ length: 40 });
+      //encrypted jwt to be used in cookie
+      const token = jwt.sign(userObj, ENDCODED_SECRET);
 
-      // //create response cookie on login
-      // res.cookie("token", token, { httpOnly: true });
+      res.cookie("token", token, { httpOnly: true });
       //redirect to databaseQuery string so front end can display data
+
       res.locals.redirectUrl = databaseQuery;
       next();
     } catch (err) {}
@@ -120,5 +106,31 @@ googleController.getCredentials = async (req, res, next) => {
     });
   }
 };
+
+//function used to check if new user needs to be created
+async function checkUser(currentUser) {
+  try {
+    //check if user is currently in database
+    let user = await User.HabitsData.find({ userId: currentUser.user_id });
+    //if user is an empty array, user was not found so create new user and return user id
+    if (Array.isArray(user) && user.length === 0) {
+      //create new User query string out of passed in currentUser object
+      //should be changed to /dashboard enpoint
+      const newUserQuery = `http://localhost:3000/dashboard?${queryString.stringify(
+        currentUser
+      )}`;
+      return newUserQuery;
+    } else {
+      //if user does exist return a query string with users id
+      //should be changed to /dashboard enpoint
+      const oldUserQuery = `http://localhost:3000/dashboard?${queryString.stringify(
+        currentUser.user_id
+      )}`;
+      return oldUserQuery;
+    }
+  } catch (err) {
+    return err;
+  }
+}
 
 module.exports = googleController;
--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ const app = express();
 //routers
 const googleLoginRouter = require("./routes/googleLogin.js");
 const databaseRouter = require("./routes/database.js");
+const dashboardRouter = require("./routes/dashboard");
 
 const PORT = 3000;
 // const cors = require("cors")
 
-//cookie parser that will be used to persist user sessionsc
+//cookie parser that will be used to persist user sessions
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
@@ -18,15 +19,32 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//request from frontend to database
 app.use("/database", databaseRouter);
 
+//send static webpack build bundle
 app.use("/build", express.static(path.join(__dirname, "../build")));
 
 //for login requests
 app.use("/login", googleLoginRouter);
 
+//for request to habit dashboard
+app.use("/dashboard", dashboardRouter);
+
+//for requests to frontend home page
 app.get("/", (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, "../client/index.html"));
+});
+
+//global error handler setup
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: "There was an error in a middleware function",
+    status: 400,
+    message: { err: "An error occured in a middlewear function" },
+  };
+  const errObj = Object.assign(defaultErr, { err: err });
+  return res.status(errObj.status).send(errObj.message);
 });
 
 app.listen(PORT, () => {
