d21ab979e5e89c518dc51c84c2e96b45649eb459
Chao Yu <ChaoY91@yahoo.com>
2021-09-20 00:09:31 -0400

    updated profile after log in

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -5,6 +5,7 @@ class UserProfile extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      userId: 21312322222, 
       user: 'Pikachu',
       profilePic: 'https://media.wired.com/photos/5f87340d114b38fa1f8339f9/master/w_1600%2Cc_limit/Ideas_Surprised_Pikachu_HD.jpg',
       /* user_id: this.props.user_id,
@@ -13,8 +14,17 @@ class UserProfile extends Component {
          */ 
     }
   }
+
   //=======Gets user data after login page, wg
   componentDidMount(){
+     const urlParams = window.location.search;
+     const urlUser = new URLSearchParams(urlParams)
+     const userId = urlUser.get('user_id')
+     console.log(userId);
+     const user = urlUser.get('email')
+     console.log(user);
+     console.log('test')
+    this.setState({user: user, userId: userId})
     //toServer.getUser(url/cookie)
   }
 
