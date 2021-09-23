5488397dd6091615d9b73bf85f0445bd9943460a
Chao Yu <ChaoY91@yahoo.com>
2021-09-23 00:00:58 -0400

    added DB testing

--- server/__tests__/supertest.js
+++ server/__tests__/supertest.js
@@ -1,9 +1,10 @@
 const request = require('supertest');
 const server = 'http://localhost:3000';
-
+const express = require('express');
 const mongoose = require('mongoose');
 const databaseName = 'test';
-const User =require('../models/userModels')
+const User = require('../models/habitModels');
+
 
 describe('Route integration', () => {
   //login page
@@ -17,8 +18,7 @@ describe('Route integration', () => {
       });
     });
 
-    //interview page
-    describe('/database', () => {
+      describe('/', () => {
       describe('GET', () => {
         it('responds with a 200 status and a json object', () =>
           request(server)
@@ -30,15 +30,38 @@ describe('Route integration', () => {
   });
 });
 
-beforeAll(async () => {
-  const url =
-    'mongodb+srv://ChaoY:Codesmith@cluster0.zjvee.mongodb.net/tracker?retryWrites=true&w=majority';
-  await mongoose
-    .connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      dbName: 'test',
-    })
-    .then(() => console.log('Connected to test DB'))
-    .catch((err) => console.log(err));
+describe('Database functionality', () => {
+  //
+  beforeAll(async () => {
+    const url =
+      'mongodb+srv://ChaoY:Codesmith@cluster0.zjvee.mongodb.net/habitApp?retryWrites=true&w=majority';
+    await mongoose
+      .connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'habitcards',
+      })
+      .then(() => console.log('Connected to test DB'))
+      .catch((err) => console.log(err));
+  });
+
+  // afterEach(async () => {
+  //   await User.deleteMany();
+  // });
+
+  it('Create new user', async () => {
+    const res = await request(server)
+      .post('/database/checkUser?user_id=123&name=test&email=testing@gmail.com')
+      .send({ user_id: '123', name: 'Test', email: 'testing@gmail.com' })
+     .expect(User.findOne({ email: 'testing@gmail.com' })).toBe();
+  });
+
+  it('Return a user', () => {
+    request(server)
+      .get('database/?user_id=123')
+      .expect(200)
+      .expect('Content-type', /application\/json/)
+      .expect(res.body.user_id).toBe()
+  });
+
 });
--- server/models/userModels.js
+++ server/models/userModels.js
@@ -13,7 +13,7 @@ mongoose
   .catch((error) => console.log(`Error connecting to database, ${error}`));
 
 const userSchema = new mongoose.Schema({
-  user_id: { type: Number, required },
+  user_id: { type: Number, required: true },
   user_name: { type: String, required: true },
   user_email: { type: String, required: true },
   password: { type: String },
